d5c187188acee0fa55ea76fe9db2c86df8ff4721
GigaJake474 <jacoseke474@gmail.com>
2024-12-08 10:55:40 -0500

    Finished first battle

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 #libraries
 import time
+import random
+
+#Functions
+def death():
+    print("Holy Crap! My Grandma is better at battling then ye!")
+    time.sleep(2)
+    print("GAME OVER")
 
 # Intro
 time.sleep(1.5)
@@ -16,3 +23,16 @@ else:
     time.sleep(2)
     print("THE END")
     exit(1)
+
+time.sleep(2)
+print("Ok, welcome to the arena. To survive you must defeat many violent, merciless monsters.")
+
+#Battles Start
+print("You see Grandpa Goblin. He is very short so you can just kick him like its the opening of the Super Bowl")
+input("Type a number between 1 and 20.")
+random_int = random.randint(1,20)
+if random_int == input:
+    death()
+else:
+    time.sleep(1)
+    print("O.K. But dont be to proud, my grandma fights better then ye an she's dead!")
